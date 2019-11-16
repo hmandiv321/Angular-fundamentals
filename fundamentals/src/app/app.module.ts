6b2030e0b9555e232ac5f0178284fa738a49402a
hmandiv321 <hmandiv@outlook.com>
2019-11-16 17:57:53 -0500

    add authors component and service

--- fundamentals/src/app/app.module.ts
+++ fundamentals/src/app/app.module.ts
@@ -1,20 +1,25 @@
+import { AuthorsService } from './authors.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
+import { AuthorsComponent } from './authors/authors.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseComponent
+    CourseComponent,
+    AuthorsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    AuthorsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
