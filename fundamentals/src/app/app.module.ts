83c66d161f37cc02bd771a73c7b085ca3b4ba059
hmandiv321 <hmandiv@outlook.com>
2019-11-20 18:11:03 -0500

    Exercise for custom pipe

--- fundamentals/src/app/app.module.ts
+++ fundamentals/src/app/app.module.ts
@@ -4,10 +4,12 @@ import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TitleCasePipe } from './title-case.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleCasePipe
   ],
   imports: [
     BrowserModule,
