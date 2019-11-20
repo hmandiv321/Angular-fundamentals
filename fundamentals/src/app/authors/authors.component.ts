0b4fe6a2b5e81b08227a70698ace70799ffadead
hmandiv321 <hmandiv@outlook.com>
2019-11-19 22:21:38 -0500

    add built-in pipe examples

--- fundamentals/src/app/authors/authors.component.ts
+++ fundamentals/src/app/authors/authors.component.ts
@@ -12,28 +12,13 @@ export class AuthorsComponent implements OnInit {
   constructor(service: AuthorsService) {
     this.authors = service.getAuthors();
    }
-   /* If this email variable ispopulated eg. email = "me@example"
-   then the input field will show me@example as the html has
-   <input [value]="email" (keyup.enter)="onKeyUp()">
-   But with only property binding aka "one way binding"
-   changes to from the dom wont be reflected in the component
-   we use two way binding for that
-   */
-  // Two-way binding
-  /**
-   * Angular special syntax for two way binding==>
-   * <input [(ngModel)]="email" (keyup.enter)="onKeyUp()">
-   */
-   email = 'me@example.com';
-   twoWay = 'me@example.com';
-   onKeyUpOneWayBinding() {
-    console.log('One way binding', this.email);
-   }
-
-   onKeyUpTwoWayBinding() {
-    console.log('Two way binding', this.twoWay);
-   }
-
+   Test = {
+     title: 'Test',
+     commaExample: 301324,
+     rating: 4.7654,
+     price: 450.68,
+     date: new Date(2019, 11, 19)
+   };
   ngOnInit() {
   }
 
