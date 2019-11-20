ad2c4415db90c4c4d4b76b8c044cf5a0e816931f
hmandiv321 <hmandiv@outlook.com>
2019-11-19 20:21:35 -0500

    add template variable exapmles

--- fundamentals/src/app/authors/authors.component.ts
+++ fundamentals/src/app/authors/authors.component.ts
@@ -12,17 +12,9 @@ export class AuthorsComponent implements OnInit {
   constructor(service: AuthorsService) {
     this.authors = service.getAuthors();
    }
-   // angular way no need for the $event to be passed
-   onKeyUp() {
-    // Traditional way of implementing this
-    /* <input (keyup)="onKeyUp($event)">
-    if ($event.keyCode === 13) {
-      console.log('Enter was pressed');
-    }*/
-
-    // Angular way in html
-    /*<input (keyup.enter)="onKeyUp()"> */
-    console.log('Enter was pressed');
+   // Template Variables
+   onKeyUp(email) {
+    console.log(email);
    }
 
   ngOnInit() {
