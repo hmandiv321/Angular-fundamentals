f26fde8df755153f2e91ca5c6fd0acb371c95917
hmandiv321 <hmandiv@outlook.com>
2019-11-20 14:40:11 -0500

    remove favourite component

--- fundamentals/src/app/app.module.ts
+++ fundamentals/src/app/app.module.ts
@@ -4,12 +4,10 @@ import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FavouriteComponent } from './favourite/favourite.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FavouriteComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
