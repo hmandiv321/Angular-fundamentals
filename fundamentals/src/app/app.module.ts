202eecca7146879ef6e093ea8dcd6db9046f75de
hmandiv321 <hmandiv@outlook.com>
2019-11-21 21:16:55 -0500

    remove panel component

--- fundamentals/src/app/app.module.ts
+++ fundamentals/src/app/app.module.ts
@@ -4,12 +4,10 @@ import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PanelComponent } from './panel/panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PanelComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
