6d0038880b323d131c9629ec178e7537a28dfcdb
hmandiv321 <hmandiv@outlook.com>
2019-11-22 19:29:05 -0500

    basic ngFor example

--- fundamentals/src/app/app.component.ts
+++ fundamentals/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  viewMode = 'map';
+  topics = [
+    {id: 1, name: 'topic1'},
+    {id: 2, name: 'topic2'},
+    {id: 3, name: 'topic3'},
+    {id: 4, name: 'topic4'}
+  ];
 }
