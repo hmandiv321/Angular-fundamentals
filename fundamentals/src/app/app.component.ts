0cb6c0958b3372f44bd6c1175816e78337c78332
hmandiv321 <hmandiv@outlook.com>
2019-11-22 19:01:21 -0500

    basic ngSwitchCase example

--- fundamentals/src/app/app.component.ts
+++ fundamentals/src/app/app.component.ts
@@ -6,10 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  courses = [];
-
-  doWeHaveAnyCourses() {
-    return this.courses.length > 0;
-  }
+  viewMode = 'map';
 }
