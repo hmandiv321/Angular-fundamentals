d50284f41c57bbe7cd6fce943f051a403fcf87c8
hmandiv321 <hmandiv@outlook.com>
2019-11-22 23:08:03 -0500

    remove input-format directive

--- fundamentals/src/app/app.module.ts
+++ fundamentals/src/app/app.module.ts
@@ -4,12 +4,10 @@ import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InputFormatDirective } from './input-format.directive';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    InputFormatDirective
+    AppComponent
   ],
   imports: [
     BrowserModule,
