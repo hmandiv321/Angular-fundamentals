6a09ff417e7b7c5a85e2ae68f8403df956787017
hmandiv321 <hmandiv@outlook.com>
2019-11-24 14:06:18 -0500

    remove course-form

--- fundamentals/src/app/app.module.ts
+++ fundamentals/src/app/app.module.ts
@@ -4,12 +4,10 @@ import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CourseFormComponent } from './course-form/course-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CourseFormComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
