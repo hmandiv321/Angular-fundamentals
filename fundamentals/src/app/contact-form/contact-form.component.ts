71c053b55dba031722f73725d3bbd3c9b4f6f595
hmandiv321 <hmandiv@outlook.com>
2019-11-23 19:57:21 -0500

    add ngForm 'sumbit form values as json' Example

--- fundamentals/src/app/contact-form/contact-form.component.ts
+++ fundamentals/src/app/contact-form/contact-form.component.ts
@@ -6,8 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent {
-
-  log(input) {
-    console.log(input);
+  submit(form) {
+    console.log(form.value);
   }
 }
