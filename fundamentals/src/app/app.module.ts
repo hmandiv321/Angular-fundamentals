3baae7d6a157d7d1b5304d2f8f6146b6fa3cb56a
hmandiv321 <hmandiv@outlook.com>
2019-11-24 14:04:39 -0500

    add template-driven fully functional form example

--- fundamentals/src/app/app.module.ts
+++ fundamentals/src/app/app.module.ts
@@ -4,10 +4,12 @@ import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseFormComponent } from './course-form/course-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ fundamentals/src/app/course-form/course-form.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-form',
+  templateUrl: './course-form.component.html',
+  styleUrls: ['./course-form.component.css']
+})
+export class CourseFormComponent {
+categories = [
+  {id: 1, name: 'Development'},
+  {id: 2, name: 'Art'},
+  {id: 3, name: 'Languages'},
+];
+onSubmit(form) {
+  console.log(form.value);
+}
+}
