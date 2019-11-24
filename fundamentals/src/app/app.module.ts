e9f98216039507e5340f71ae3dd60bb886bd8245
hmandiv321 <hmandiv@outlook.com>
2019-11-23 23:12:08 -0500

    remove contact-form example

--- fundamentals/src/app/app.module.ts
+++ fundamentals/src/app/app.module.ts
@@ -4,12 +4,10 @@ import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ContactFormComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
