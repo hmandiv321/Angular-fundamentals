eecbd5526b3632189240315662d5f00ee6657789
hmandiv321 <hmandiv@outlook.com>
2019-11-24 18:10:13 -0500

    add signup-form example 'start of reactive-forms here'

--- fundamentals/src/app/app.module.ts
+++ fundamentals/src/app/app.module.ts
@@ -4,10 +4,12 @@ import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupFormComponent
   ],
   imports: [
     BrowserModule,
