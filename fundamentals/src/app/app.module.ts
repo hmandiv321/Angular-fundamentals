c8c96f4b68818bccee0fc128e2b91bb7926df853
hmandiv321 <hmandiv@outlook.com>
2019-11-25 13:38:48 -0500

    add change password example using reactive form

--- fundamentals/src/app/app.module.ts
+++ fundamentals/src/app/app.module.ts
@@ -4,10 +4,12 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChangePasswordComponent
   ],
   imports: [
     BrowserModule,
