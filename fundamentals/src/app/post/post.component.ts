aa07491e5fefa159b4ffb8a625977b13c700f66f
hmandiv321 <hmandiv@outlook.com>
2019-11-25 17:59:41 -0500

    add http post update data example

--- fundamentals/src/app/post/post.component.ts
+++ fundamentals/src/app/post/post.component.ts
@@ -26,4 +26,10 @@ export class PostComponent {
           console.log(response);
         });
   }
+  updatePost(post) {
+    this.http.patch(this.url + '/' + post.id, JSON.stringify({isRead: true}))
+	.subscribe(response => {
+          console.log(response);
+        });
+  }
 }
