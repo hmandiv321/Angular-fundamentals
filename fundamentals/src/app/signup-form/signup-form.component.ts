cd528d1b6509f7629ceb1b86ad989aa11a3573ad
hmandiv321 <hmandiv@outlook.com>
2019-11-24 20:28:54 -0500

    add custom validation implementation example

--- fundamentals/src/app/signup-form/signup-form.component.ts
+++ fundamentals/src/app/signup-form/signup-form.component.ts
@@ -1,3 +1,4 @@
+import { UsernameValidators } from './username.validators';
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
@@ -10,7 +11,8 @@ export class SignupFormComponent {
   form = new FormGroup({
     username: new FormControl('', [
       Validators.required,
-      Validators.minLength(5)
+      Validators.minLength(5),
+      UsernameValidators.cannotContainSpace
     ]),
     password: new FormControl('', [
       Validators.required,
