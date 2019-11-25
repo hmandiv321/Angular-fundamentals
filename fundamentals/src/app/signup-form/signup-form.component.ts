6ab80107a3e10f13ade53f435fa25bde4c6ff9a0
hmandiv321 <hmandiv@outlook.com>
2019-11-24 22:52:44 -0500

    add validatng upon sumiting the form example

--- fundamentals/src/app/signup-form/signup-form.component.ts
+++ fundamentals/src/app/signup-form/signup-form.component.ts
@@ -19,6 +19,22 @@ export class SignupFormComponent {
       Validators.minLength(8),
     ])
   });
+  login() {
+    this.form.setErrors({
+      invalidLogin: true
+    });
+    /* an example
+    let isValid = authService.login(this.form.value);
+    if (isValid === false ) {
+      // form level errors
+       this.form.setErrors({
+         invalidLogin: true
+       });
+
+      // individual errors
+      // this.username.setErrors;
+    }*/
+  }
   get username() {
     return this.form.get('username');
   }
