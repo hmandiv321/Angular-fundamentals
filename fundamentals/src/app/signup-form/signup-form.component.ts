b0a8ca42c1200941d059c90a35cf62439dd8b5f3
hmandiv321 <hmandiv@outlook.com>
2019-11-24 23:06:21 -0500

    add nested formGroup example

--- fundamentals/src/app/signup-form/signup-form.component.ts
+++ fundamentals/src/app/signup-form/signup-form.component.ts
@@ -9,36 +9,28 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class SignupFormComponent {
   form = new FormGroup({
-    username: new FormControl('', [
-      Validators.required,
-      Validators.minLength(5),
-      UsernameValidators.cannotContainSpace
-    ], UsernameValidators.shouldBeUnique),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.minLength(8),
-    ])
+    // nested formGroup example
+    account: new FormGroup({
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5),
+        UsernameValidators.cannotContainSpace
+      ], UsernameValidators.shouldBeUnique),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ])
+    }),
   });
   login() {
     this.form.setErrors({
       invalidLogin: true
     });
-    /* an example
-    let isValid = authService.login(this.form.value);
-    if (isValid === false ) {
-      // form level errors
-       this.form.setErrors({
-         invalidLogin: true
-       });
-
-      // individual errors
-      // this.username.setErrors;
-    }*/
   }
   get username() {
-    return this.form.get('username');
+    return this.form.get('account.username');
   }
   get password() {
-    return this.form.get('password');
+    return this.form.get('account.password');
   }
 }
