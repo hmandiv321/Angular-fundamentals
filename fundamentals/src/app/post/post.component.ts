26865405a1af7171799f69e878d3186e797baa30
hmandiv321 <hmandiv@outlook.com>
2019-12-06 21:03:50 -0500

    merge conflict check

--- fundamentals/src/app/post/post.component.ts
+++ fundamentals/src/app/post/post.component.ts
@@ -15,29 +15,4 @@ export class PostComponent {
         this.posts = response;
       });
   }
-
-  createPost(input: HTMLInputElement) {
-    const post = {title: input.value};
-    input.value = '';
-    this.http.post(this.url, JSON.stringify(post))
-        .subscribe(response => {
-          post['id'] = response;
-          this.posts.splice(0, 0, post);
-          console.log(response);
-        });
-  }
-  updatePost(post) {
-    this.http.patch(this.url + '/' + post.id, JSON.stringify({isRead: true}))
-	.subscribe(response => {
-          console.log(response);
-        });
-  }
-  deletePost(post) {
-    this.http.delete(this.url + '/' + post.id)
-	.subscribe(response => {
-	  let index = this.posts.indexOf(post);
-	  this.posts.splice(index, 1);
-         // console.log(response);
-        });
-  }
 }
